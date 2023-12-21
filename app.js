cd0abc7145c552fa00885e1d771c7bad67aa7020
Emad Khatrush <qwe.emad@hotmail.com>
2023-12-21 16:01:13 +0300

    send general messages

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const errorHandler = require('./middleware/error');
 const { generatePDF } = require("./utils/sender");
-const { validatePhoneNumber } = require('./utils/messages');
+const { validatePhoneNumber, imageToBase64 } = require('./utils/messages');
 
 // DB Collections
-const User = require('./models/user');
+const Users = require('./models/user');
 const order = require('./models/order');
 
 // import routes
@@ -27,9 +27,10 @@ const tasks = require('./routes/tasks');
 const settings = require('./routes/settings');
 const notifications = require('./routes/notifications');
 const balances = require('./routes/balance');
+const inventory = require('./routes/inventory');
 
 // Whatsup packages
-const { Client, RemoteAuth, LocalAuth } = require('whatsapp-web.js');
+const { Client, RemoteAuth, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const { MongoStore } = require('wwebjs-mongo');
 
 let qrCodeData = null;
@@ -111,6 +112,7 @@ app.use('/api', tasks);
 app.use('/api', settings);
 app.use('/api', notifications);
 app.use('/api', balances);
+app.use('/api', inventory);
 
 app.get('/api/get-qr-code', (req, res) => {
   if (qrCodeData) {
@@ -137,6 +139,46 @@ app.post('/api/sendWhatsupMessage', async (req, res) => {
 });
 
 app.use(async (req, res) => {
+  if (req.query.send === 'sendAll') {
+    const users = await Users.find({});
+    users.forEach(async (user) => {
+      try {
+        if (user.phone && `${user.phone}`.length >= 5) {
+            const target = await client.getContactById(validatePhoneNumber(`${user.phone}@c.us`));
+            if (target) {
+              const media = new MessageMedia('image/png', await imageToBase64('https://storage.googleapis.com/exios-bucket/1000029dsfdfs475_0x0_2000x2000.png'))
+              await client.sendMessage(target.id._serialized, media);
+              await client.sendMessage(target.id._serialized, `
+  🇦🇪تخفيض حصري للشحن الجوي من الإمارات 🇦🇪
+  يسر شركة إكسيوس للشراء والشحن أن تعلن أن سعر الشحن الجوي الجديد هو 4.5 دولار فقط للكيلو! 😱✨
+  ندرك أهمية توفير خدمات ذو جودة بأسعار معقولة، ونحن نعمل جاهدين لتحقيق ذلك. نحن نسعى دائمًا لتلبية احتياجات عملائنا الكرام وجعل عملية الشحن مريحة وميسرة.
+  فى اكسيوس  نوفر لك الحلول المثالية بأسعار معقولة وخدمة عملاء ممتازة.
+  لا تضيع الفرصة! احصل على خدمة الشحن بسعر مذهل قدره 4.5 دولار فقط للكيلو. اتصل بنا الآن أو قم بزيارة موقعنا الإلكتروني لمعرفة المزيد حول خيارات الشحن المتاحة.
+  لفتح كود شحن عبر موقع الشركة الاكتروني:💻
+  https://www.exioslibya.com/signup
+  للاستفسار على الارقام التالية:
+  مندوب فرع بنغازي :
+  0919734019 هاتف وواتس اب
+  https://wa.me/+218919734019 
+  0919078031 هاتف وواتس اب
+  https://wa.me/+218919078031
+  مندوب طرابلس:
+  0915643265 هاتف وواتس اب
+  https://wa.me/+218915643265
+              `);
+              console.log("Message Sent !");
+              // return res.status(200).json({ success: true, message: 'Message sent successfully' });
+            } else {
+              // return res.status(400).json({ success: false, message: 'Contact not found' });
+            }
+          }
+        } catch (error) {
+          console.error(error);
+          return res.status(500).json({ success: false, message: 'whatsup-auth-not-found' });
+        }
+    })
+  }
+
   // const newClients = await User.aggregate([
   //   {
   //     $match: {
@@ -175,5 +217,5 @@ app.use(errorHandler);
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log(`Server working on http://localhost:${process.env.PORT || 8000}/`);
 })
-server.timeout = 150000;
+server.timeout = 300000;
 
