36fd27f5add9f4a704ba88db536815eaf57a6823
Emad Khatrush <qwe.emad@hotmail.com>
2024-07-27 14:03:25 +0300

    fix whatsup

--- app.js
+++ app.js
@@ -74,20 +74,15 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log('MongoDB connected');
   const store = new MongoStore({ mongoose: mongoose });
-  const WhatsAppConfig = process.env.NODE_ENV !== "production" ? LocalAuth : RemoteAuth;
+  const WhatsAppConfig = process.env.NODE_ENV !== "production" ? LocalAuth : LocalAuth;
   client = new Client({
     authStrategy: new WhatsAppConfig({
-      clientId: 'admin-client-2',
+      clientId: 'adminClient',
       store,
-      backupSyncIntervalMs: 300000
     }),
     puppeteer: {
       headless: true,
       args: ['--no-sandbox'],
-    },
-    webVersionCache: {
-      type: "remote",
-      remotePath: "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.4-beta.html",
     },
   });
   client.initialize();
