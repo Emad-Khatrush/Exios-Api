03dd65fd8b0f982c2087393e738975e92901339b
Emad Khatrush <qwe.emad@hotmail.com>
2024-12-22 20:19:21 +0300

    Deploy

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
   console.log('MongoDB connected');
   const store = new MongoStore({ mongoose: mongoose });
-  const WhatsAppConfig = RemoteAuth;
+  const WhatsAppConfig = LocalAuth;
   client = new Client({
     authStrategy: new WhatsAppConfig({
       store,
