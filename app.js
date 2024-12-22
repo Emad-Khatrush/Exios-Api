212652f34aa4555577681b41f318bf3a2576fea9
Emad Khatrush <qwe.emad@hotmail.com>
2024-12-22 19:39:00 +0300

    Deploy

--- app.js
+++ app.js
@@ -136,34 +136,17 @@ db.once("open", () => {
       backupSyncIntervalMs: 300000
     }),
     puppeteer: {
+      headless: true,
       args: [
         "--no-sandbox",
         "--disable-setuid-sandbox",
-        "--log-level=3",
-        "--no-default-browser-check",
-        "--disable-site-isolation-trials",
-        "--no-experiments",
-        "--ignore-gpu-blacklist",
-        "--ignore-certificate-errors",
-        "--ignore-certificate-errors-spki-list",
-        "--enable-gpu",
-        // "--disable-extensions",
-        "--disable-default-apps",
-        "--enable-features=NetworkService",
-        "--disable-webgl",
-        "--disable-threaded-animation",
-        "--disable-threaded-scrolling",
-        "--disable-in-process-stack-traces",
-        "--disable-histogram-customizer",
-        "--disable-gl-extensions",
-        "--disable-composited-antialiasing",
-        "--disable-canvas-aa",
-        "--disable-3d-apis",
+        "--disable-dev-shm-usage",
         "--disable-accelerated-2d-canvas",
-        "--disable-accelerated-jpeg-decoding",
-        "--disable-accelerated-mjpeg-decode",
-        "--disable-app-list-dismiss-on-blur",
-        "--disable-accelerated-video-decode"
+        "--disable-gpu",
+        "--no-zygote",
+        "--single-process",
+        "--disable-background-timer-throttling",
+        "--disable-renderer-backgrounding"
       ]
     }
   });
